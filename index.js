f30aa505635db28c4be35f7a1a9c6134ea64108b
721377 <nonelabide@gmail.com>
2023-08-19 13:27:50 +0000

    home-project-menu

---
+++ index.js
@@ -0,0 +1,25 @@
+var menu = document.querySelector('.menu');
+const nav = document.querySelector('nav');
+const naclo = document.querySelector('.close');
+const clos = document.getElementById('clo');
+const close1 = document.getElementById('close1');
+const close2 = document.getElementById('close2');
+const close3 = document.getElementById('close3');
+const close4 = document.getElementById('close4');
+
+function closen() {
+  nav.style.visibility = "hidden";
+  nav.style.opacity = "0";
+}
+
+menu.onclick = function() {
+  nav.style.visibility = "visible";
+  nav.style.opacity = "1";
+};
+
+naclo.onclick = closen; // Remove parentheses
+clos.onclick = closen;
+close1.onclick = closen;
+close2.onclick = closen;
+close3.onclick = closen;
+close4.onclick = closen;
